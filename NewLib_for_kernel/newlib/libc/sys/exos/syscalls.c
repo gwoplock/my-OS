a006e560bfcdd386dcc8f517494b9b3215927622
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-08-09 16:10:45 -0500

    newlib side of syscalls this should work, hopefully, maybe, please. also found 2 new syscalls in the list that i didn't see. gave them numbers 20-21. see spreadsheet

--- NewLib_for_kernel/newlib/libc/sys/exos/syscalls.c
+++ NewLib_for_kernel/newlib/libc/sys/exos/syscalls.c
@@ -7,46 +7,95 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <errno.h>
+#include <stdarg.h>
 #undef errno
 extern int errno;
 
-void _exit(){
+void _exit( ) {
+	void* args[] = { };
+	syscallHelper(1, args);
 }
-int close(int file){
+int close(int file) {
+	void* args[] = { &file };
+	return sysCallHelper(2, args);
 }
 char *__env[1] = { 0 };
 char **environ = __env; /* pointer to array of char * strings that define the current environment variables */
-int execve(char *name, char **argv, char **env){
+int execve(char *name, char **argv, char **env) {
+	void* args[] = { name, argv, env };
+	return sysCallHelper(4, args);
 }
-int fork(){
+int fork( ) {
+	void* args[] = { };
+	return sysCallHelper(5, args);
 }
-int fstat(int file, struct stat *st){
+int fstat(int file, struct stat *st) {
+	void* args[] = { &file, st };
+	return sysCallHelper(6, args);
 }
-int getpid(){
+int getpid( ) {
+	void* args[] = { };
+	return sysCallHelper(7, args);
 }
-int isatty(int file){
+int isatty(int file) {
+	void* args[] = { &file };
+	return sysCallHelper(9, args);
 }
-int kill(int pid, int sig){
+int kill(int pid, int sig) {
+	void* args[] = { &pid, &sig };
+	return sysCallHelper(10, args);
 }
-int link(char *old, char *new){
+int link(char *old, char *new) {
+	void* args[] = { old, new };
+	return sysCallHelper(11, args);
 }
-int lseek(int file, int ptr, int dir){
+int lseek(int file, int ptr, int dir) {
+	void* args[] = { &file, &prt, &dir };
+	return sysCallHelper(12, args);
 }
 int open(const char *name, int flags, int mode) {
+	void* args[] = { name, &flags, &mode };
+	return sysCallHelper(13, args);
 }
-int read(int file, char *ptr, int len){
+int read(int file, char *ptr, int len) {
+	void* args[] = { &file, ptr, &len };
+	return sysCallHelper(14, args);
 }
-caddr_t sbrk(int incr){
+caddr_t sbrk(int incr) {
+	void* args[] = { &incr };
+	return sysCallHelper(15, args);
 }
-int stat(const char *file, struct stat *st){
+clock_t times(struct tms *buf) {
+	void* args[] = { buf };
+	return sysCallHelper(16, args);
 }
-clock_t times(struct tms *buf){
+int unlink(char *name) {
+	void* args[] = { name };
+	return sysCallHelper(17, args);
 }
-int unlink(char *name){
+int wait(int *status) {
+	void* args[] = { status };
+	return sysCallHelper(18, args);
 }
-int wait(int *status){
+int write(int file, char *ptr, int len) {
+	void* args[] = { &file, ptr, &len };
+	return sysCallHelper(19, args);
 }
-int write(int file, char *ptr, int len){
+int gettimeofday(struct timeval *p, struct timezone *z) {
+	void* args[] = { p, z };
+	return sysCallHelper(20, args);
 }
-int gettimeofday(struct timeval *p, struct timezone *z){
+
+int stat(const char *file, struct stat *st) {
+	void* args[] = { file, st };
+	return sysCallHelper(21, args);
+}
+void* sysCallHelper(int sysCallNum, void** args) {
+	void* ret;
+	asm ("push %0": : "m" (args));
+	asm ("push %0":: "r" (sysCallNum));
+	asm ("int $0xE0");
+	//todo i think this may be wrong...
+	asm (pop %0, "m"(ret));
+	return ret;
 }
