cee477d17d19d45eeca233f9c6f78cf1dafb3c8e
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-08-10 12:56:28 -0500

    my midnight revilation. i dont need to return the pointer b/c its at a known address. also i forgor to move the stack when i was done

--- NewLib_for_kernel/newlib/libc/sys/exos/syscalls.c
+++ NewLib_for_kernel/newlib/libc/sys/exos/syscalls.c
@@ -13,91 +13,108 @@ extern int errno;
 
 void* sysCallHelper(int sysCallNum, void** args);
 
+void* scractchSpace = 0xFFFFF000;
+
 void _exit( exitCode) {
 	void* args[] = {&exitCode};
 	syscallHelper(1, args);
 }
 int close(int file) {
 	void* args[] = { &file };
-	return sysCallHelper(2, args);
+	sysCallHelper(2, args);
 }
 char *__env[1] = { 0 };
 char **environ = __env; /* pointer to array of char * strings that define the current environment variables */
 int execve(char *name, char **argv, char **env) {
 	void* args[] = { name, argv, env };
-	return sysCallHelper(4, args);
+	sysCallHelper(4, args);
+	return &((int*) scratchSpace);
 }
 int fork( ) {
 	void* args[] = { };
-	return sysCallHelper(5, args);
+	sysCallHelper(5, args);
+	return &((int*) scratchSpace);
 }
 int fstat(int file, struct stat *st) {
 	void* args[] = { &file, st };
-	return sysCallHelper(6, args);
+	sysCallHelper(6, args);
+	return &((int*) scratchSpace);
 }
 int getpid( ) {
 	void* args[] = { };
-	return sysCallHelper(7, args);
+	sysCallHelper(7, args);
+	return &((int*) scratchSpace);
 }
 int isatty(int file) {
 	void* args[] = { &file };
-	return sysCallHelper(9, args);
+	sysCallHelper(9, args);
+	return &((int*) scratchSpace);
 }
 int kill(int pid, int sig) {
 	void* args[] = { &pid, &sig };
-	return sysCallHelper(10, args);
+	sysCallHelper(10, args);
+	return &((int*) scratchSpace);
 }
 int link(char *old, char *new) {
 	void* args[] = { old, new };
-	return sysCallHelper(11, args);
+	sysCallHelper(11, args);
+	return &((int*) scratchSpace);
 }
 int lseek(int file, int ptr, int dir) {
 	void* args[] = { &file, &prt, &dir };
-	return sysCallHelper(12, args);
+	sysCallHelper(12, args);
+	return &((int*) scratchSpace);
 }
 int open(const char *name, int flags, int mode) {
 	void* args[] = { name, &flags, &mode };
-	return sysCallHelper(13, args);
+	sysCallHelper(13, args);
+	return &((int*) scratchSpace);
 }
 int read(int file, char *ptr, int len) {
 	void* args[] = { &file, ptr, &len };
-	return sysCallHelper(14, args);
+	sysCallHelper(14, args);
+	return &((int*) scratchSpace);
 }
 caddr_t sbrk(int incr) {
 	void* args[] = { &incr };
-	return sysCallHelper(15, args);
+	sysCallHelper(15, args);
+	return &((caddr_t*) scratchSpace);
 }
 clock_t times(struct tms *buf) {
 	void* args[] = { buf };
-	return sysCallHelper(16, args);
+	sysCallHelper(16, args);
+	return &((clock_t*) scratchSpace);
 }
 int unlink(char *name) {
 	void* args[] = { name };
-	return sysCallHelper(17, args);
+	sysCallHelper(17, args);
+	return &((int*) scratchSpace);
 }
 int wait(int *status) {
 	void* args[] = { status };
-	return sysCallHelper(18, args);
+	sysCallHelper(18, args);
+	return &((int*) scratchSpace);
 }
 int write(int file, char *ptr, int len) {
 	void* args[] = { &file, ptr, &len };
-	return sysCallHelper(19, args);
+	sysCallHelper(19, args);
+	return &((int*) scratchSpace);
 }
 int gettimeofday(struct timeval *p, struct timezone *z) {
 	void* args[] = { p, z };
-	return sysCallHelper(20, args);
+	sysCallHelper(20, args);
+	return &((int*) scratchSpace);
 }
 //TODO check def of stuct stat
 int stat(const char *file, struct stat *st) {
 	void* args[] = { file, st };
-	return sysCallHelper(21, args);
+	sysCallHelper(21, args);
+	return &((int*) scratchSpace);
 }
-void* sysCallHelper(int sysCallNum, void** args) {
-	void* ret;
+void sysCallHelper(int sysCallNum, void** args) {
 	asm ("push %0": : "m" (args));
 	asm ("push %0":: "r" (sysCallNum));
 	asm ("int $0xE0");
-	//todo i think this may be wrong...
-	asm (pop %0, "m"(ret));
-	return ret;
+	//todo check operand direction
+	asm ("add %%ESP, 8");
 }
