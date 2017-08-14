dde96cf9cbb5f816908cdbdde0692c6287631ff5
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-08-14 08:08:59 -0500

    sbrk we "should" be done with porting newlib. we will see where it explodes and fix those next.

--- NewLib_for_kernel/newlib/libc/sys/exos/syscalls.c
+++ NewLib_for_kernel/newlib/libc/sys/exos/syscalls.c
@@ -7,7 +7,6 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <errno.h>
-#include <stdarg.h>
 #undef errno
 extern int errno;
 
@@ -28,88 +27,88 @@ char **environ = __env; /* pointer to array of char * strings that define the cu
 int execve(char *name, char **argv, char **env) {
 	void* args[] = { name, argv, env };
 	sysCallHelper(4, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int fork( ) {
 	void* args[] = { };
 	sysCallHelper(5, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int fstat(int file, struct stat *st) {
 	void* args[] = { &file, st };
 	sysCallHelper(6, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int getpid( ) {
 	void* args[] = { };
 	sysCallHelper(7, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int isatty(int file) {
 	void* args[] = { &file };
 	sysCallHelper(9, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int kill(int pid, int sig) {
 	void* args[] = { &pid, &sig };
 	sysCallHelper(10, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int link(char *old, char *new) {
 	void* args[] = { old, new };
 	sysCallHelper(11, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int lseek(int file, int ptr, int dir) {
 	void* args[] = { &file, &prt, &dir };
 	sysCallHelper(12, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int open(const char *name, int flags, int mode) {
 	void* args[] = { name, &flags, &mode };
 	sysCallHelper(13, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int read(int file, char *ptr, int len) {
 	void* args[] = { &file, ptr, &len };
 	sysCallHelper(14, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 caddr_t sbrk(int incr) {
 	void* args[] = { &incr };
 	sysCallHelper(15, args);
-	return &((caddr_t*) scratchSpace);
+	return *((caddr_t*) scratchSpace);
 }
 clock_t times(struct tms *buf) {
 	void* args[] = { buf };
 	sysCallHelper(16, args);
-	return &((clock_t*) scratchSpace);
+	return *((clock_t*) scratchSpace);
 }
 int unlink(char *name) {
 	void* args[] = { name };
 	sysCallHelper(17, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int wait(int *status) {
 	void* args[] = { status };
 	sysCallHelper(18, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int write(int file, char *ptr, int len) {
 	void* args[] = { &file, ptr, &len };
 	sysCallHelper(19, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 int gettimeofday(struct timeval *p, struct timezone *z) {
 	void* args[] = { p, z };
 	sysCallHelper(20, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 //TODO check def of stuct stat
 int stat(const char *file, struct stat *st) {
 	void* args[] = { file, st };
 	sysCallHelper(21, args);
-	return &((int*) scratchSpace);
+	return *((int*) scratchSpace);
 }
 void sysCallHelper(int sysCallNum, void** args) {
 	asm ("push %0": : "m" (args));
